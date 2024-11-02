90a4f100a2f3cf2d7865a61b7dcd1e34567bd493
francis <krypton@gmail.com>
2024-11-02 16:02:08 +0100

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,10 +9,12 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """BasicCache inherits from BaseCaching with no caching limit."""
     def put(self, key, item):
+        """Add an item to the cache."""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """Get an item by key from the cache."""
         if key is not None:
             return None
         return self.cache_data.get(key)
