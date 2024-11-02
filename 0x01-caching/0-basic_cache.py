1e4de3fb00c3b337cb679e8bf3be8ef89b5c2f25
francis <krypton@gmail.com>
2024-11-02 15:59:54 +0100

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,6 +7,7 @@ This module provides a basic caching system with no item limit.
 from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
+    """BasicCache inherits from BaseCaching with no caching limit."""
     def put(self, key, item):
         if key is not None and item is not None:
             self.cache_data[key] = item
